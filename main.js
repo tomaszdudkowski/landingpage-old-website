891ac469c0445fedefe98dce15b4f0e8aae72531
tomaszszd <tomaszszd@gmail.com>
2021-12-17 12:58:26 +0100

    Add toggler for navbar. Add navbar Bootstrap.

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 // main js file
 
 function scrollFunction() {
-    let navbar = document.getElementById("navbar");
+    let navbar = document.getElementByIdName("navbarh");
     let navbar_btn = document.getElementById("navbar-btn");
 
     if (
       document.body.scrollTop > 80 ||
       document.documentElement.scrollTop > 80
     ) {
-      navbar.style.height = "5.3rem";
-      navbar.classList.add("navbar-link");
-      navbar.style.backgroundColor = "#fff";
+      //navbar.classList.add("navbarh-link");
+      navbar.classList.remove("navbar-dark");
+      navbar.classList.add("bg-light");
+      navbar.classList.add("navbar-light")
       navbar_btn.classList.remove("btn-s");
       navbar_btn.classList.remove("btn");
       navbar_btn.classList.remove("btn-round");
       navbar_btn.classList.remove("btn-glow");
     } else {
-      navbar.style.height = "6rem";
-      navbar.classList.remove("navbar-link");
+      //navbar.classList.remove("navbarh-link");
+      navbar.classList.remove("navbar-light");
+      navbar.classList.add("bg-light");
+      navbar.classList.add("navbar-dark")
       navbar.style.backgroundColor = "rgba(56, 57, 66, 0)";
       navbar.style.color = "#FFF";
       navbar_btn.classList.add("btn");
       navbar_btn.classList.add("btn-s");
-      navbar.btn.classList.add("btn-round");
+      navbar_btn.classList.add("btn-round");
       navbar_btn.classList.add("btn-glow");
     }
   }
