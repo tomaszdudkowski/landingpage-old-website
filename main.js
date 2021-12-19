c5dada8469b0603bd6ae1b396b35d409de9e89ab
tomaszdudkowski <tomaszszd@gmail.com>
2021-12-19 17:29:52 +0100

    Fix main.js with style for navbar form bootstrap.

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // main js file
 
 function scrollFunction() {
-    let navbar = document.getElementByIdName("navbarh");
+    let navbar = document.getElementById("navbarh");
     let navbar_btn = document.getElementById("navbar-btn");
 
     if (
       document.body.scrollTop > 80 ||
       document.documentElement.scrollTop > 80
     ) {
-      //navbar.classList.add("navbarh-link");
-      navbar.classList.remove("navbar-dark");
+      navbar.classList.add("navbar-light");
       navbar.classList.add("bg-light");
-      navbar.classList.add("navbar-light")
+      navbar.classList.remove("navbarh-link");
+      navbar.classList.remove("navbar-dark");
       navbar_btn.classList.remove("btn-s");
       navbar_btn.classList.remove("btn");
       navbar_btn.classList.remove("btn-round");
       navbar_btn.classList.remove("btn-glow");
     } else {
-      //navbar.classList.remove("navbarh-link");
+      navbar.classList.add("navbarh-link");
+      navbar.classList.add("navbar-dark");
+      navbar.classList.remove("bg-light");
       navbar.classList.remove("navbar-light");
-      navbar.classList.add("bg-light");
-      navbar.classList.add("navbar-dark")
       navbar.style.backgroundColor = "rgba(56, 57, 66, 0)";
       navbar.style.color = "#FFF";
       navbar_btn.classList.add("btn");
